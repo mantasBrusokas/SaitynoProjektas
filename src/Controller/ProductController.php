2e9bf5c011ed9c26b69e3776715ef3831660e202
Mantas Brusokas <mantasbrusokas@gmail.com>
2020-10-02 20:40:42 +0300

    Initial commit

---
+++ src/Controller/ProductController.php
@@ -0,0 +1,194 @@
+<?php
+namespace App\Controller;
+
+
+use App\Entity\Product;
+use App\Repository\ProductRepository;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\Routing\Annotation\Route;
+
+/**
+ * Class ProductController
+ * @package App\Controller
+ * @Route("/api", name="product_api")
+ */
+class ProductController extends AbstractController
+{
+    /**
+     * @param ProductRepository $productRepository
+     * @return JsonResponse
+     * @Route("/products", name="products", methods={"GET"})
+     */
+    public function getProducts(ProductRepository $productRepository){
+        $data = $productRepository->findAll();
+        return $this->response($data);
+    }
+
+    /**
+     * @param Request $request
+     * @param EntityManagerInterface $entityManager
+     * @param ProductRepository $productRepository
+     * @return JsonResponse
+     * @throws \Exception
+     * @Route("/products", name="products_add", methods={"POST"})
+     */
+    public function addProduct(Request $request, EntityManagerInterface $entityManager, ProductRepository $productRepository){
+
+        try{
+            $request = $this->transformJsonBody($request);
+
+            if (!$request || !$request->get('name') || !$request->request->get('description')){
+                throw new \Exception();
+            }
+
+            $product = new Product();
+            $product->setName($request->get('name'));
+            $product->setDescription($request->get('description'));
+            $product->setCreateDate(new \DateTime($request->get('time')));
+            $entityManager->persist($product);
+            $entityManager->flush();
+
+            $data = [
+                'status' => 200,
+                'success' => "Product added successfully",
+            ];
+            return $this->response($data);
+
+        }catch (\Exception $e){
+            $data = [
+                'status' => 422,
+                'errors' => "Data no valid",
+            ];
+            return $this->response($data, 422);
+        }
+
+    }
+
+
+    /**
+     * @param ProductRepository $productRepository
+     * @param $id
+     * @return JsonResponse
+     * @Route("/products/{id}", name="products_get", methods={"GET"})
+     */
+    public function getProduct(ProductRepository $productRepository, $id){
+        $product = $productRepository->find($id);
+
+        if (!$product){
+            $data = [
+                'status' => 404,
+                'errors' => "Product not found",
+            ];
+            return $this->response($data, 404);
+        }
+        return $this->response($product);
+    }
+
+    /**
+     * @param Request $request
+     * @param EntityManagerInterface $entityManager
+     * @param ProductRepository $productRepository
+     * @param $id
+     * @return JsonResponse
+     * @Route("/products/{id}", name="products_put", methods={"PUT"})
+     */
+    public function updateProduct(Request $request, EntityManagerInterface $entityManager, ProductRepository $productRepository, $id){
+
+        try{
+            $product = $productRepository->find($id);
+
+            if (!$product){
+                $data = [
+                    'status' => 404,
+                    'errors' => "Product not found",
+                ];
+                return $this->response($data, 404);
+            }
+
+            $request = $this->transformJsonBody($request);
+
+            if (!$request || !$request->get('name') || !$request->request->get('description')){
+                throw new \Exception();
+            }
+
+            $product->setName($request->get('name'));
+            $product->setDescription($request->get('description'));
+            $entityManager->flush();
+
+            $data = [
+                'status' => 200,
+                'errors' => "Product updated successfully",
+            ];
+            return $this->response($data);
+
+        }catch (\Exception $e){
+            $data = [
+                'status' => 422,
+                'errors' => "Data no valid",
+            ];
+            return $this->response($data, 422);
+        }
+
+    }
+
+
+    /**
+     * @param ProductRepository $productRepository
+     * @param $id
+     * @return JsonResponse
+     * @Route("/products/{id}", name="products_delete", methods={"DELETE"})
+     */
+    public function deleteProduct(EntityManagerInterface $entityManager, ProductRepository $productRepository, $id){
+        $product = $productRepository->find($id);
+
+        if (!$product){
+            $data = [
+                'status' => 404,
+                'errors' => "Product not found",
+            ];
+            return $this->response($data, 404);
+        }
+
+        $entityManager->remove($product);
+        $entityManager->flush();
+        $data = [
+            'status' => 200,
+            'errors' => "Product deleted successfully",
+        ];
+        return $this->response($data);
+    }
+
+
+
+
+
+    /**
+     * Returns a JSON response
+     *
+     * @param array $data
+     * @param $status
+     * @param array $headers
+     * @return JsonResponse
+     */
+    public function response($data, $status = 200, $headers = [])
+    {
+        return new JsonResponse($data, $status, $headers);
+    }
+
+    protected function transformJsonBody(\Symfony\Component\HttpFoundation\Request $request)
+    {
+        $data = json_decode($request->getContent(), true);
+
+        if ($data === null) {
+            return $request;
+        }
+
+        $request->request->replace($data);
+
+        return $request;
+    }
+
+}
